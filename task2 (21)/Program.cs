a9c7add29e79a41b30d692240715c96249c06c7c
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-09 04:08:20 +0300

    добавила решение второй и третьей задачи

---
+++ task2 (21)/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+void FillArray(int[] collection)
+{
+    int index = 0;
+    while (index < 3)
+    {
+        Console.Write("кордината точки:");
+        collection[index] =  int.Parse(Console.ReadLine());
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int position = 0;
+    while (position < 3)
+    {
+        if (position == 2)
+        {
+            Console.WriteLine(col[position]);
+        }
+        else 
+        {
+            Console.Write(col[position] + " ");
+        }
+        position++;
+    }
+}
+
+int[] array1 = new int [3]; //определила массив 1
+int[] array2 = new int [3]; //определила массив 2
+
+Console.WriteLine("введите координаты точки №1 в трехмерном пространстве:");
+FillArray(array1); //вызвала функцию заполнения массива
+Console.WriteLine("координаты точки №1:");
+PrintArray(array1); //вызвала функцию печати массива
+
+Console.WriteLine("введите координаты точки №2 в трехмерном пространстве:");
+FillArray(array2); //вызвала функцию заполнения массива
+Console.WriteLine("координаты точки №2:");
+PrintArray(array2); //вызвала функцию печати массива
+
+
+double dist;
+Console.Write("расстояние между двумя точками равно:");
+dist = Math.Sqrt(Math.Pow((array2[0]-array1[0]), 2) + Math.Pow((array2[1]-array1[1]), 2) + Math.Pow((array2[2]-array1[2]), 2));
+Console.WriteLine(Math.Round(dist, 2));
---
+++ task3 (23)/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        if (position == (count - 1))
+        {
+            Console.Write(col[position] + ".");
+        }
+        else
+        {
+            Console.Write(col[position] + ", ");
+        }
+        position++;
+    }
+}
+
+Console.WriteLine("Введите число:");
+int N;
+while (!int.TryParse(Console.ReadLine(), out N))
+{
+    System.Console.WriteLine("Введите корректное значение:");
+}
+
+int i = 0;
+int n = 1;
+int[] array = new int[N];
+while (n <= N)
+{
+    array[i] = n*n*n;
+    i++;
+    n++;
+}
+
+PrintArray(array);
